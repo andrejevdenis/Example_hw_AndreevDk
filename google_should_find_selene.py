d8bbe3140039de00c009407309947adfc6e6c7f0
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-13 15:02:12 +0400

    Autorisation positive

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,6 +1,12 @@
+from asyncio import wait_for
+
 from selene import browser, be, have
 
 
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('налог ру').press_enter()
-browser.element('[id="search"]').should(have.text('Федеральная налоговая служба'))
+browser.element('[href="https://www.nalog.gov.ru/"]').press_enter()
+browser.element('[href="https://lkfl2.nalog.ru/lkfl/login"]').press_enter()
+browser.element('[id="username_1"]').type('631108262591')
+browser.element('[id="password_2"]').type('1').press_enter()
+browser.element('[data-qa="header-container"]').should(have.text('Личный'))
